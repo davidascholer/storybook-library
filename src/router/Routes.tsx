ca205925a8cb6b384b5d69c537c4820fab872232
David Scholer <davidascholer@gmail.com>
2025-01-11 20:51:37 -0800

    updated routes

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -7,21 +7,26 @@ import SignOut from "@/app/auth/SignOut";
 import ForgotPassword from "@/app/auth/ForgotPassword";
 import NotFound from "@/app/NotFound";
 import AuthLayout from "@/app/auth/AuthLayout";
+import Sidebar from "@/app/Sidebar";
+import Settings from "@/app/Settings";
 
 function Routes() {
   return (
     <AppRoutes>
-      <Route path="*" element={<NotFound />} />
-      <Route index element={<Home />} />
-      <Route path="query" element={<Query />} />
-      <Route path="auth">
-        <Route element={<AuthLayout />}>
-          <Route index element={<SignIn />} />
-          <Route path="sign-out" element={<SignOut />} />
-          <Route path="sign-up" element={<SignUp />} />
-          <Route path="forgot-password" element={<ForgotPassword />} />
+      <Route element={<Sidebar />}>
+        <Route index element={<Home />} />
+        <Route path="query" element={<Query />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="auth">
+          <Route element={<AuthLayout />}>
+            <Route index element={<SignIn />} />
+            <Route path="sign-out" element={<SignOut />} />
+            <Route path="sign-up" element={<SignUp />} />
+            <Route path="forgot-password" element={<ForgotPassword />} />
+          </Route>
         </Route>
       </Route>
+      <Route path="*" element={<NotFound />} />
     </AppRoutes>
   );
 }
