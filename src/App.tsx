dbc7b02385fb8942c071482f23c9e3f5d09fd7c4
David Scholer <davidascholer@gmail.com>
2025-01-12 01:09:06 -0800

    modified routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { ThemeProvider } from "./theme/ThemeProvider";
 import RouterManager from "./router/RouterManager";
 import Routes from "./router/Routes";
+import ReduxProvider from "./state/ReduxProvider";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterManager>
-        <Routes />
-      </RouterManager>
-    </ThemeProvider>
+    <ReduxProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterManager>
+          <Routes />
+        </RouterManager>
+      </ThemeProvider>
+    </ReduxProvider>
   );
 }
 
--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,6 +1,6 @@
 import { Routes as AppRoutes, Route } from "react-router";
 import Home from "@/app/Home";
-import Query from "@/app/Query";
+import QueryList from "@/app/QueryList";
 import SignUp from "@/app/auth/SignUp";
 import SignIn from "@/app/auth/SignIn";
 import SignOut from "@/app/auth/SignOut";
@@ -10,13 +10,19 @@ import AuthLayout from "@/app/auth/AuthLayout";
 import Sidebar from "@/app/Sidebar";
 import Settings from "@/app/Settings";
 import paths from "./paths";
+import QueryDetail from "@/app/QueryDetail";
 
 function Routes() {
   return (
     <AppRoutes>
       <Route element={<Sidebar />}>
         <Route index element={<Home />} />
-        <Route path={paths.query} element={<Query />} />
+        
+        <Route path={paths.query}>
+          <Route index element={<QueryList />} />
+          <Route path=":pokemonName" element={<QueryDetail />} />
+        </Route>
+
         <Route path={paths.settings} element={<Settings />} />
         <Route path={paths.auth.root}>
           <Route element={<AuthLayout />}>
