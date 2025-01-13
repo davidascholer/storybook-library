fd2800f7bd9945b3f6cfcbdfb98e9ea818ce93df
David Scholer <davidascholer@gmail.com>
2025-01-12 16:06:45 -0800

    sidebar icon size change

--- src/components/ui/sidebar/SidebarTrigger.tsx
+++ src/components/ui/sidebar/SidebarTrigger.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { cn } from "@/lib/utils";
-import { PanelLeft } from "lucide-react";
+import { Menu } from "lucide-react";
 import useSidebar from "./hooks/useSidebar";
 import { Button } from "@/components/ui/button/Button";
 
@@ -16,14 +16,14 @@ const SidebarTrigger = React.forwardRef<
       data-sidebar="trigger"
       variant="ghost"
       size="icon"
-      className={cn("h-7 w-7", className)}
+      className={cn("h-16 w-16", className)}
       onClick={(event) => {
         onClick?.(event);
         toggleSidebar();
       }}
       {...props}
     >
-      <PanelLeft />
+      <Menu />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   );
